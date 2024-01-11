7ad65bb44e7c75e180f5dddaaf353b71822051d1
Alexillo98 <musateduardalexandru@gmail.com>
2024-01-11 10:34:54 +0100

    Clase humano

---
+++ src/Human.java
@@ -0,0 +1,30 @@
+public class Human implements Comparable<Human>
+{
+    private String nombre;
+    private int salario;
+
+    public Human (String nombre, int salario)
+    {
+        this.nombre = nombre;
+        this.salario = salario;
+    }
+
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+
+    public int getSalario()
+    {
+        return this.salario;
+    }
+
+    public String toString()
+    {
+        return this.nombre + " " + this.salario;
+    }
+    public int compareTo (Human other)
+    {
+        return other.getSalario() - this.salario;
+    }
+}
