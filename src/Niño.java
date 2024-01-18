32785cc1d909d130ef4a0cb634da739c7156bea7
Alexillo98 <musateduardalexandru@gmail.com>
2024-01-18 10:19:08 +0100

    Niño clase

--- src/Niño.java
+++ src/Niño.java
@@ -2,6 +2,22 @@ public class Niño implements Comparable<Niño>
 {
     private int bueno;
 
+    public int getBueno() {
+        return bueno;
+    }
+
+    public void setBueno(int bueno) {
+        this.bueno = bueno;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
     private int peso;
 
     public Niño(int bueno, int peso)
@@ -10,16 +26,20 @@ public class Niño implements Comparable<Niño>
         this.peso = peso;
     }
 
+
     public String toString(){return this.bueno + " " + this.peso;}
 
     public int compareTo (Niño otro)
     {
-        if (this.bueno == otro.bueno)
+        if (this.bueno == otro.getBueno())
         {
-            return this.peso - otro.peso;
+            //return this.peso - otro.getPeso();
+            if (this.peso == otro.getPeso()) return 0;
+            if (this.peso > otro.getPeso()) return 1;
+            return -1;
         }else
         {
-            return otro.bueno - this.bueno;
+            return otro.getBueno() - this.bueno;
         }
     }
 }
